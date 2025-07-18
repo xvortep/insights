15528e687c98095420e08cb04136d23e2e3da0e7
Александар Петровски <97363871+xvortep@users.noreply.github.com>
2025-07-18 14:59:34 +0200

    Update variator.cpp

--- variant/variator.cpp
+++ variant/variator.cpp
@@ -1,60 +1,84 @@
-// https://godbolt.org/z/8n9dYh45h
+// https://godbolt.org/z/jxGxjE4K3
 
-#include <iostream>
 #include <cstdint>
 #include <cstring>
-#include <memory>
 #include <functional>
-#include <variant>
-#include <map>
 #include <iostream>
+#include <map>
+#include <memory>
+#include <variant>
 
-template<class t>
+template <class def, class... types>
 struct variator {
+    variator(def obj) : holder(obj) {}
 
-    variator(t& obj) : holder(obj) {}
-    variator(int obj) : holder(obj) {}
-
-    t operator[](int a) {
-        return *std::get<t>(holder).next;
+    template <class t>
+    variator(t obj) : holder(obj) {
+        static_assert(
+            std::is_same_v<t, def> || (std::is_same_v<t, types> || ...),
+            "the type requested is not supported for this template");
     }
 
-    operator int() {
-        return std::get<int>(holder);
+    variator<def, types...> operator[](std::string str) { return std::get<def>(holder)[str]; }
+
+    template <class t>
+    operator t() {
+        static_assert(
+            std::is_same_v<t, def> || (std::is_same_v<t, types> || ...),
+            "the type requested is not supported for this template");
+
+        return std::get<t>(holder);
     }
 
-    std::variant<int, t> holder;
+    std::variant<def, types...> holder;
 };
 
 struct type {
-    type(int a) : value(a) {}
-    
     type* next = nullptr;
 
     int value;
+    char character;
 
+    // default backup type has to be first
+    using v_t = variator<type, int, char>;
 
-    variator<type> operator[](int a) {
-        if (!a)
+    v_t operator[](std::string a) {
+        if (a == "")
             return *next;
-        else
+        else if (a == "v")
             return next->value;
+        else if (a == "c")
+            return next->character;
+        else
+            return *next;
     }
 };
 
-
-
-
 int main() {
-    type a(1), s(2), d(3), f(4);
+    type a, s, d, f;
 
     a.next = &s;
     s.next = &d;
     d.next = &f;
 
-    int h = a[0][0][1];
+    a.value = 1;
+    s.value = 2;
+    d.value = 3;
+    f.value = 4;
+
+    a.character = 'a';
+    s.character = 's';
+    d.character = 'd';
+    f.character = 'f';
+
+    int h = a[""][""]["v"];
+    char c = a[""]["c"];
+
+    // this should fail
+    // h = a["c"];
 
     std::cout << h << std::endl;
+    std::cout << c << std::endl;
 
     return 0;
 }
